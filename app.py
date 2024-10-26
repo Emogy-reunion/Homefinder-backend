54e4463c8d09a7dec53b7081868eed3044f58738
Emogy-reunion <mv7786986@gmail.com>
2024-10-26 09:31:34 +0300

    fix the typo error to fix the module import error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Handles login management, creating initial admins and running the application
 '''
 from create_app import create_app
 from model import db, bcrypt, Users
-from utils.verfication import mail
+from utils.verification import mail
 from routes.authentication import auth
 from flask_migrate import Migrate
 
