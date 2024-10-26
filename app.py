6a368760e108a03e0d983381ca81c2aa1b9e514a
Emogy-reunion <mv7786986@gmail.com>
2024-10-26 09:06:42 +0300

    configure flask migrate for migration purposes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from create_app import create_app
 from model import db, bcrypt, Users
 from utils.verfication import mail
 from routes.authentication import auth
+from flask_migrate import Migrate
 
 
 app = create_app()
@@ -18,12 +19,12 @@ db.init_app(app)
 bcrypt.init_app(app)
 mail.init_app(app)
 
+migrate = Migrate(app, db)
+
 app.register_blueprint(auth)
 
 with app.app_context():
     db.create_all()
-
-create_models()
 
 if __name__ == '__main__':
     app.run(debug=True)
