527b95b5a2d2014073a845834ea3ab974a9071f6
Emogy-reunion <mv7786986@gmail.com>
2024-11-11 05:09:19 +0300

    create a function that resends the verification link to users

---
+++ utils/resend.py
@@ -0,0 +1,16 @@
+from flask import render_template, url_for
+from flask_mail import Message
+from utils.verification import mail
+
+def resend_verification_email(user):
+
+    token = user.generate_token()
+    verification_url = url_for('verify_email', token=token, _external=True)
+    msg = Message(
+            subject='Identity verification',
+            sender='info.bytevision@gmail.com',
+            recipients=[user.email]
+            )
+    msg.body = f'Click the following link to verify your identity {verification_url}'
+    msg.html = render_template('reverification.html', name=user.firstname, verification_url=verification_url)
+    mail.send(msg)
