358ad101031ef4521dfbf7871b7483b4e3329e4e
Emogy-reunion <mv7786986@gmail.com>
2024-11-11 04:56:58 +0300

    add a route that sends reverification emails to the users

--- routes/verification.py
+++ routes/verification.py
@@ -3,6 +3,7 @@ this file contains routes that verify users and reverify them
 '''
 from flask import Blueprint, jsonify
 from models import Users, db
+from utils.resend import resend_verification_email
 
 
 verify = Blueprint('verify', __name__)
@@ -17,4 +18,19 @@ def verify_email(token):
         db.session.commit()
         return jsonify({'success': 'Email verified successfuly!'})
     else:
-        return jsonify({'Error': 'Verification failed, try again'})
+        return jsonify({'error': 'Verification failed, try again!'})
+
+@verify.route('/resend_verification_email', methods=['POST'])
+def resend_verification_email():
+
+    data = request.json()
+    email = data['email']
+
+    user = Users.query.filter_by(email=email).first()
+
+    if not user:
+        return jsonfiy({'error': 'There is no account associated with this email!'})
+    else:
+        resend_verification_email(user)
+        return jsonify({"error": "Please check your email for a verification link to complete the registration process!"})
+
