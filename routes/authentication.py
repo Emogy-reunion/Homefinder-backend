8a06afe5be155354bab064415437c6aaeaa72f86
Emogy-reunion <mv7786986@gmail.com>
2025-04-02 14:51:13 +0300

    fix: handle validation checks by validating the email, first name, last name, add error codes to help flag errors easily

--- routes/authentication.py
+++ routes/authentication.py
@@ -4,8 +4,8 @@ This file contains routes that handle user authentication
 from flask import Blueprint, request, render_template, redirect, jsonify
 from model import db, Users
 from utils.verification import send_verification_email
+from utils.validation import validate_firstname, validate_lastname, check_email
 import re
-from email_validator import validate_email, EmailNotValidError
 from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, jwt_required, get_jwt_identity, unset_jwt_cookies
 
 auth = Blueprint('auth', __name__)
@@ -18,7 +18,7 @@ def register():
     It extractst the data and saves it to the database
     '''
     data = request.json
-    errors = {}
+
 
     firstname = data.get('firstname').lower()
     lastname = data.get('lastname').lower()
@@ -26,56 +26,46 @@ def register():
     email = data.get('email').lower()
     password = data.get('password')
 
-    if not firstname:
-        errors['firstname'] = 'First name is required!'
 
-    if not lastname:
-        errors['lastname'] = 'Lastname is required!'
+    errors = {}
 
-    if not email:
-        errors['email'] = 'Email is required!'
-    else:
-        try:
-            valid = validate_email(email)
-        except EmailNotValidError as e:
-            errors['email'] = str(e)
+    firstname_errors = validate_firstname(firstname)
+    lastname_errors = validate_lastname(lastname)
+    email_errors = check_email(email)
 
-    if not agency:
-        errors['agency'] = 'Agency is required!'
-        
-    if not password:
-        errors['password'] = 'Password is required!'
-    elif len(password) < 8:
-        errors['password'] = 'Password must be at least 8 characters long!'
-    elif not re.search(r"[A-Z]", password):
-        errors['password'] = 'Password must contain at least one uppercase letter!'
-    elif not re.search(r"[a-z]", password):
-        errors['password'] = 'Password must contain at least one lowercase letter!'
-    elif not re.search(r"[0-9]", password):
-        errors['password'] = 'Password must contain at least one digit!'
-    elif not re.search(r"[@$!%*?&]", password):
-        errors['password'] = 'Password must contain at least one special character (@$!%*?&)!'
+    if firstname_errors:
+        errors['firstname'] = firstname_errors
+
+    if lastname_errors:
+        errors['lastname'] = lastname_errors
+
+    if email_errors:
+        errors['email'] = email_errors
 
     if errors:
-        return jsonify({'errors': errors})
+        return jsonify({'errors': errors}), 400
 
+    user = None
 
-    user = Users.query.filter_by(email=email).first()
+    try:
+        user = Users.query.filter_by(email=email).first()
+    except Exception as e:
+        return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
 
     if user:
-        return jsonify({'error': 'An account associated with this email exists'})
+        return jsonify({'error': 'An account associated with this email exists'}), 409
     else:
         try:
             new_user = Users(firstname=firstname, lastname=lastname, 
                              agency=agency, email=email, password=password)
             db.session.add(new_user)
+            db.session.commit()
         except Exception as e:
             db.session.rollback()
-            return jsonify({'error': 'An unexpected error occured. Try again!'})
-        db.session.commit()
+            return jsonify({'error': 'An unexpected error occured. Please try again!'}), 500
 
         send_verification_email(new_user)
-        return jsonify({'success': 'Your account has been successfully created. Please check your email for a verification link to complete the registration process.'})
+        return jsonify({'success': 'Your account has been successfully created. Please check your email for a verification link to complete the registration process.'}), 201
 
 
 @auth.route('/login', methods=['POST'])
